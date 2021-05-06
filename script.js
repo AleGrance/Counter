90b74359d07f2a35628d0a33f9042f9665a9f3c0
AleGrance <alegrance1000@hotmail.com>
2021-05-06 16:52:43 -0400

    Se agrega uikit

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
+// La fecha
+let date = new Date();
 // Contador
 let contador = 0;
 
 // Seleccionar valores
-
 const value = document.querySelector('#value');
-const btns = document.querySelectorAll(".btn");
+const fecha = document.querySelector('#fecha');
+const btns = document.querySelectorAll(".uk-button");
 
-btns.forEach(function (item) {
-    item.addEventListener("click", function (e) {
-        //console.log();
+fecha.textContent = date;
+
+btns.forEach(function(item) {
+    item.addEventListener("click", function(e) {
+        //console.log(item);
 
         const style = e.currentTarget.classList;
-        if(style.contains('descontar')) {
+        if (style.contains('descontar')) {
             contador--;
         } else if (style.contains('aumentar')) {
             contador++;
         } else {
             contador = 0;
         }
-        if(contador > 0) {
-            value.style.color = "green"; 
+        if (contador > 0) {
+            value.style.color = "green";
         }
-        if(contador < 0) {
-            value.style.color = "red"; 
+        if (contador < 0) {
+            value.style.color = "red";
         }
-        if(contador === 0) {
-            value.style.color = "#222"; 
+        if (contador === 0) {
+            value.style.color = "#222";
         }
         value.textContent = contador;
     })
